4b7a3da6fba03bfca369491772bdbd71e8a0ffa6
seungjinyu93 <seungjin.yu@nway.com>
2025-06-05 11:39:13 -0400

    renewal for resnet18

--- resnet/18/resnet18.py
+++ resnet/18/resnet18.py
@@ -6,14 +6,48 @@ import torchvision
 import torchvision.transforms as transforms
 from torchvision.models import resnet18
 from torch.utils.data import DataLoader
+import platform
+
+
+device = ""
+
+if platform.system() == "Darwin":
+    device = torch.device("mps" if torch.cuda.is_available() else "cpu")
+elif platform.system() == "Linux":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def print_param_and_grad_stats(model):
+    total_params = 0
+    total_grads = 0
+    total_param_bytes = 0
+    total_grad_bytes = 0
+
+    for name, param in model.named_parameters():
+        numel = param.numel()
+        grad_numel = param.grad.numel() if param.grad is not None else 0
+        total_params += numel
+        total_grads += grad_numel
+
+        param_bytes = numel * 4 / 1024 / 1024  # float32 = 4 bytes
+        grad_bytes = grad_numel * 4 / 1024 / 1024
+
+        total_param_bytes += param_bytes
+        total_grad_bytes += grad_bytes
+
+        print(f"{name:<40} {str(param.shape):<25} - param: {param_bytes:.2f} MB, grad: {grad_bytes:.2f} MB")
+
+    print(f"\n[Epoch Summary]")
+    print(f"Total Parameters: {total_params:,} - {total_param_bytes:.2f} MB")
+    print(f"Total Gradients : {total_grads:,} - {total_grad_bytes:.2f} MB")
+
 
 
 def main() :
     # setting parameters
     batch_size = 128
     learning_rate = 0.01
-    num_epochs = 10
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    num_epochs = 1
+    print(f"Running on {platform.system()} and using {device}")
 
     # CIFAR-10 for transform
     transform = transforms.Compose([
@@ -23,8 +57,8 @@ def main() :
     ])
 
     # dataset loading 
-    train_dataset = torchvision.datasets.CIFAR10(root="./data",train=True, transform=transform, download=True)
-    test_dataset = torchvision.datasets.CIFAR10(root="./data",train=False, transform=transform, download= True)
+    train_dataset = torchvision.datasets.CIFAR10(root="../data",train=True, transform=transform, download=True)
+    test_dataset = torchvision.datasets.CIFAR10(root="../data",train=False, transform=transform, download= True)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size,shuffle=True,num_workers=2)
     test_loader = DataLoader(test_dataset, batch_size=batch_size,shuffle=True,num_workers=2)
@@ -53,6 +87,8 @@ def main() :
             running_loss += loss.item()
 
         print(f"Epoch {epoch+1}/{num_epochs}, Loss:{running_loss/len(train_loader):.4f}")
+
+        print_param_and_grad_stats(model)
 
         model.eval()
         correct , total = 0,0
